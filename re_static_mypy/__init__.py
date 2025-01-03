c4a4342e5bed5de27e4b3ca1c62fc24057225725
mingargus <oleg@tryargus.com>
2025-01-03 23:51:46 +0000

    feat(analyzer): base implementation

--- re_static_mypy/__init__.py
+++ re_static_mypy/__init__.py
@@ -1,6 +1,21 @@
 __version__ = "0.0.0"
 __version_tuple__ = (0, 0, 0)
 
+# from re import Pattern as _Pattern, compile as _compile
+# from typing import Annotated, AnyStr
+# from typing_extensions import TypeAlias
+
+
+# _static = object()
+# Pattern: TypeAlias = Annotated[_Pattern[AnyStr], _static]
+
+
+# def compile(
+#     pattern: AnyStr,
+#     flags: int = 0,
+# ) -> Pattern[AnyStr]:
+#     return _compile(pattern=pattern, flags=flags)
+
 
 def some_function() -> str:
     """
---
+++ re_static_mypy/mypy_plugin/plugin.py
@@ -0,0 +1,10 @@
+# from mypy.plugin import Plugin
+
+
+# class ReStaticMypyPlugin(Plugin):
+#     def get_type_analyze_hook(self, fullname: str):
+#         ...
+
+
+# def plugin(version: str):
+#     return ReStaticMypyPlugin
